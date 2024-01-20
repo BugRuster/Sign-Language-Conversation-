e3a4ef218fa0318d67a031ed008ccafb10ab2e31
Preet Taparia <preettaparia@jklu.edu.in>
2024-01-20 15:49:36 +0530

    integration phase started

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import io
@@ -6,9 +6,11 @@ import base64
 from PIL import Image
 import cv2
 import numpy as np
-from flask import Flask, render_template
 from webcam_detect import sign_detection
-
+import speech_recognition as sr
+import numpy as np
+from PIL import Image
+import string
 
 
 app = Flask(__name__)
@@ -45,9 +47,8 @@ def image(data_image):
     socketio.emit('processed_frame', info)
     
 
-@app.route('/detect', methods=['POST', 'GET'])
+@app.route('/stv', methods=['POST', 'GET'])
 def detect():
-
     return render_template('index.html')
 
 @app.route('/about', methods=['POST', 'GET'])
@@ -58,7 +59,56 @@ def about():
 def landing():
     return render_template('landing.html')
 
+@app.route('/vts')
+def index():
+    return render_template('vtsl.html')
 
+@app.route('/process_audio', methods=['GET', 'POST'])
+def process_audio():
+    print('Request received!')
+    r = sr.Recognizer()
+    isl_gif = ['any questions', 'are you angry', 'are you busy', 'are you hungry', 'are you sick', 'be careful',
+                'can we meet tomorrow', 'did you book tickets', 'did you finish homework', 'do you go to office', 'do you have money',
+                'do you want something to drink', 'do you want tea or coffee', 'do you watch TV', 'dont worry', 'flower is beautiful',
+                'good afternoon', 'good evening', 'good morning', 'good night', 'good question', 'had your lunch', 'happy journey',
+                'hello what is your name', 'how many people are there in your family', 'i am a clerk', 'i am bore doing nothing',
+                 'i am fine', 'i am sorry', 'i am thinking', 'i am tired', 'i dont understand anything', 'i go to a theatre', 'i love to shop',
+                'i had to say something but i forgot', 'i have headache', 'i like pink colour', 'i live in nagpur', 'lets go for lunch', 'my mother is a homemaker',
+                'my name is john', 'nice to meet you', 'no smoking please', 'open the door', 'please call me later',
+                'please clean the room', 'please give me your pen', 'please use dustbin dont throw garbage', 'please wait for sometime', 'shall I help you',
+                'shall we go together tommorow', 'sign language interpreter', 'sit down', 'stand up', 'take care', 'there was traffic jam', 'wait I am thinking',
+                'what are you doing', 'what is the problem', 'what is todays date', 'what is your father do', 'what is your job',
+                'what is your mobile number', 'what is your name', 'whats up', 'when is your interview', 'when we will go', 'where do you stay',
+                'where is the bathroom', 'where is the police station', 'you are wrong','address','agra','ahemdabad', 'all', 'april', 'assam', 'august', 'australia', 'badoda', 'banana', 'banaras', 'banglore',
+                'bihar','bihar','bridge','cat', 'chandigarh', 'chennai', 'christmas', 'church', 'clinic', 'coconut', 'crocodile','dasara',
+                'deaf', 'december', 'deer', 'delhi', 'dollar', 'duck', 'febuary', 'friday', 'fruits', 'glass', 'grapes', 'gujrat', 'hello',
+                'hindu', 'hyderabad', 'india', 'january', 'jesus', 'job', 'july', 'july', 'karnataka', 'kerala', 'krishna', 'litre', 'mango',
+                'may', 'mile', 'monday', 'mumbai', 'museum', 'muslim', 'nagpur', 'october', 'orange', 'pakistan', 'pass', 'police station',
+                'post office', 'pune', 'punjab', 'rajasthan', 'ram', 'restaurant', 'saturday', 'september', 'shop', 'sleep', 'southafrica',
+                'story', 'sunday', 'tamil nadu', 'temperature', 'temple', 'thursday', 'toilet', 'tomato', 'town', 'tuesday', 'usa', 'village',
+                'voice', 'wednesday', 'weight','please wait for sometime','what is your mobile number','what are you doing','are you busy']
+
+    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        while True:
+            audio = r.listen(source)
+            try:
+                a = r.recognize_google(audio).lower()
+                print('You Said:', a)
+
+                for c in string.punctuation:
+                    a = a.replace(c, "")
+
+                if a in isl_gif:
+                    return jsonify({'type': 'gif', 'value': a})
+                else:
+                    for i in range(len(a)):
+                        if a[i] in arr:
+                            return jsonify({'type': 'image', 'value': a[i]})
+            except:
+                pass
 
 
 if __name__ == '__main__':
